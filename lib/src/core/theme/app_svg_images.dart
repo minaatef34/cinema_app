39cd950f9504c57a5678d99f826b1560a40fd402
David_Adel <davidadel362@gmail.com>
2025-06-10 21:42:02 +0300

    fix: auth pages

--- lib/src/core/theme/app_svg_images.dart
+++ lib/src/core/theme/app_svg_images.dart
@@ -1,5 +1,8 @@
 class AppSvgImages {
   static String appIcon = "assets/svg/app_icon.svg";
+  static String gmailIcon = "assets/svg/gmail_icon.svg";
+  static String appleIcon = "assets/svg/apple_icon.svg";
+  static String facebookIcon = "assets/svg/facebook_icon.svg";
   static String searchIcon = "assets/svg/search_light.svg";
   static String userIcon = "assets/svg/user_light.svg";
   static String playIcon = "assets/svg/play_light.svg";
@@ -8,7 +11,3 @@ class AppSvgImages {
   static String navBottomIcon2 = "assets/svg/nav_bottom_icon2.svg";
   static String navBottomIcon3 = "assets/svg/nav_bottom_icon3.svg";
   static String navBottomIcon4 = "assets/svg/nav_bottom_icon4.svg";
-  static String gmailIcon = "assets/svg/gmail_icon.svg";
-  static String appleIcon = "assets/svg/apple_icon.svg";
-  static String facebookIcon = "assets/svg/facebook_icon.svg";
-}
