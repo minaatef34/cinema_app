707d2ad743e275ab0a9bb84c0bf13826d7d06566
Mina Atef <minggkjh@gmail.com>
2025-06-11 21:28:34 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:cinema_app/firebase_options.dart';
 import 'package:cinema_app/src/core/services/package_info/package_info_service_impl.dart';
 import 'package:cinema_app/src/core/theme/app_theme.dart';
 import 'package:cinema_app/src/features/home/presentation/pages/home_page.dart';
 import 'package:cinema_app/src/features/splash/presentation/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,7 +11,9 @@ final ProviderContainer providerContainer = ProviderContainer();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await providerContainer.read(packageInfoServiceProvider).init();
 
   runApp(
